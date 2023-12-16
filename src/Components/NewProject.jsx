960c974447e507a6754829a5b07694a0b3f4c28d
samarpan sarkar <samarpansarkar209@gmail.com>
2023-12-16 20:34:51 +0530

    170

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -2,13 +2,17 @@ import Input from "./Input.jsx";
 
 export default function newProject() {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button>cancel</button>
+          <button className="text-stone-800 hover:text-stone-950">
+            cancel
+          </button>
         </li>
         <li>
-          <button>save</button>
+          <button className="px-6 cy-4 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            save
+          </button>
         </li>
       </menu>
       <div>
