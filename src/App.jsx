c1b4f0056e825bdd6512506883fb557c48bdc669
samarpan sarkar <samarpansarkar209@gmail.com>
2023-12-16 20:52:17 +0530

    171

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import ProjectsSideBar from "./Components/ProjectsSidebar";
 import { React, Fragment } from "react";
 import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
--- src/Components/ProjectsSidebar.jsx
+++ src/Components/ProjectsSidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./Button";
 
 const ProjectsSidebar = () => {
   return (
@@ -7,9 +8,10 @@ const ProjectsSidebar = () => {
         Your Projects
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+        {/* <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
           + Add Project
-        </button>
+        </button> */}
+        <Button>+ Add Project</Button>
         <ul></ul>
       </div>
     </aside>
