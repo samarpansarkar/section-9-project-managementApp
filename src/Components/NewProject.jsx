92674e6ae7d26b5329235e3c46d8d7ee7ea30ca7
samarpan sarkar <samarpansarkar209@gmail.com>
2023-12-16 22:53:37 +0530

    173

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,6 +1,26 @@
+import { useRef } from "react";
 import Input from "./Input.jsx";
 
-export default function newProject() {
+export default function newProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    //validation
+
+    console.log(enteredTitle, enteredDescription, enteredDueDate);
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -10,15 +30,18 @@ export default function newProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 cy-4 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            className="px-6 cy-4 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea={true} />
-        <Input label="Due Date" />
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea={true} />
+        <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
   );
