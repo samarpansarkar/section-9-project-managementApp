132ff9c4ec97c4d221f2a9bbeda26fb2865e5bc8
samarpan sarkar <samarpansarkar209@gmail.com>
2023-12-18 21:59:10 +0530

    176

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,15 @@ function App() {
     });
   }
 
+  function handleCancelProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
       const projectId = Math.random();
@@ -35,7 +44,9 @@ function App() {
 
   let content;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStatAddProject={handleStartAddProject} />;
   }
